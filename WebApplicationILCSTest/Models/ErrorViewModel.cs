ada1ebb6ac87697dbbd52161623aa1dfddd66504
Petrus Kuswandi <petruskuswandi1@gmail.com>
2023-09-13 13:21:15 +0700

    First Initialize

---
+++ WebApplicationILCSTest/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebApplicationILCSTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
